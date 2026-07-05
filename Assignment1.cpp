28726a26acc9a9080872b36b5707c06b35232d03
Ayush Goyal <ayushgoyal77062@gmail.com>
2026-07-05 14:48:43 +0530

    Add files via upload

---
+++ Assignment1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+     //Product of Two Numbers
+    int a,b;
+    cin>>a>>b;
+    cout<<a*b<<endl;
+    
+
+    //Type-Casting
+     a = 'U';
+    cout<<a<<endl;
+
+    //Rectangle
+     a=2,b=3;
+    cout<<a*b<<endl;
+    
+    //Product of Three Numbers or Cubes
+     a=2;
+    cout<<a*a*a<<endl;
+
+    //Size of an Integer
+     a=2;
+    cout<<sizeof(a)<<endl;
+    
+    //Swapping of Two Numbers
+     a=2,b=3;
+    cout<<a<<" "<<b<<endl; 
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout<< a<<" "<<b<<endl;
+
+    return 0;
+
+
+}
